c37431f39fda5d112b57d43d1fcb0a49a52043b5
zenlex <erichrkeil@gmail.com>
2022-04-14 20:52:25 -0500

    add favicon and fix anchor positions

---
+++ messages.js
@@ -0,0 +1,21 @@
+const msgContainer = document.getElementById('message');
+const msgBg = document.getElementById('messagebg');
+const API_URL = window.location.origin + '/api/message';
+
+const setContent = (url) => {
+  const text = fetch(url)
+    .then((response) => response.json())
+    .then(({ text }) => {
+      msgContainer.textContent = text;
+      msgBg.textContent = text;
+    });
+};
+
+//add random animation
+const animationClasses = ['gradientmask', 'waterwave', 'jumping-shadows'];
+const animindex = Math.floor(Math.random() * animationClasses.length);
+// const animindex = 0; //for debugging CSS
+msgContainer.className = animationClasses[animindex];
+msgBg.className = animationClasses[animindex];
+
+setContent(API_URL);
